0f6e7219ebf1630f91e279d6be586c9d8d96fe20
mohammedj-sadiq <mohammedj.sadiq@outlook.com>
2020-12-17 12:01:45 +0300

    moving all button to drawer

--- lib/screens/home/drawer.dart
+++ lib/screens/home/drawer.dart
@@ -1,5 +1,7 @@
 import 'package:barka/models/user.dart';
 import 'package:barka/screens/home/adding_session.dart';
+import 'package:barka/screens/home/searching_session.dart';
+import 'package:barka/services/auth.dart';
 import 'package:barka/services/database.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -13,6 +15,7 @@ class MainDrawer extends StatefulWidget {
 
 class _MainDrawerState extends State<MainDrawer> {
   String _username = '';
+  final AuthService _auth = AuthService();
 
   @override
   void initState() {
@@ -37,6 +40,17 @@ class _MainDrawerState extends State<MainDrawer> {
           });
     }
 
+    void _showSearchingForSessionModal() {
+      showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            return Container(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 60),
+              child: SearchingSession(),
+            );
+          });
+    }
+
     return Drawer(
       child: Column(
         children: [
@@ -49,43 +63,101 @@ class _MainDrawerState extends State<MainDrawer> {
                   fit: BoxFit.fill),
             ),
             width: double.infinity,
-            padding: EdgeInsets.fromLTRB(20, 140, 20, 20),
-            // color: Colors.brown[200],
-            child: Container(
-              width: double.infinity,
-              padding: EdgeInsets.only(top: 0),
-              // color: Colors.white.withOpacity(0.5),
-              decoration: BoxDecoration(
-                  border: Border.all(color: Colors.brown[900].withOpacity(0.7)),
-                  color: Colors.white.withOpacity(0.5),
-                  borderRadius: BorderRadius.all(Radius.circular(12))),
-              child: Center(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 0.0),
-                  child: Text(
-                    _username,
-                    style: TextStyle(fontSize: 20, color: Colors.black),
+            padding: EdgeInsets.fromLTRB(20, 0, 20, 20),
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.fromLTRB(262, 5, 0, 91),
+                  child: IconButton(
+                    icon: Icon(
+                      Icons.edit,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {
+                      // todo
+                      // updaing the name function
+                    },
                   ),
                 ),
-              ),
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.only(top: 0),
+                  // color: Colors.white.withOpacity(0.5),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.brown[900]),
+                      color: Colors.white.withOpacity(0.5),
+                      borderRadius: BorderRadius.all(Radius.circular(12))),
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 0.0),
+                      child: Text(
+                        _username,
+                        style: TextStyle(fontSize: 20, color: Colors.black),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
-          ListTile(
-            leading: Icon(
-              Icons.add_circle,
-              color: Colors.cyan[300],
-              size: 40,
-            ),
-            title: Text('إضافة ختمة جديدة'),
-            onTap: () {
-              _showCreatingNewSessionModal();
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.settings),
-            title: Text(
-              'profile',
-              style: TextStyle(fontSize: 18),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(8, 20, 8, 8),
+            child: Column(
+              children: [
+                ListTile(
+                  leading: Padding(
+                    padding: const EdgeInsets.only(right: 2.0),
+                    child: Icon(
+                      Icons.add_circle,
+                      color: Colors.cyan[300],
+                      size: 40,
+                    ),
+                  ),
+                  title: Text(
+                    'إضافة ختمة جديدة',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  onTap: () {
+                    _showCreatingNewSessionModal();
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 12.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.search,
+                      color: Colors.green[400],
+                      size: 40,
+                    ),
+                    title: Text(
+                      'البحث عن ختمة',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    onTap: () {
+                      _showSearchingForSessionModal();
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 392,
+                ),
+                Divider(
+                  color: Colors.brown,
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.arrow_back_ios,
+                    size: 24,
+                  ),
+                  title: Text(
+                    'تسجيل خروج',
+                    style: TextStyle(fontSize: 17),
+                  ),
+                  onTap: () async {
+                    await _auth.signOut();
+                  },
+                )
+              ],
             ),
           )
         ],
--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,10 +1,7 @@
 import 'package:barka/models/session.dart';
 import 'package:barka/models/user.dart';
-import 'package:barka/screens/home/adding_session.dart';
 import 'package:barka/screens/home/drawer.dart';
-import 'package:barka/screens/home/searching_session.dart';
 import 'package:barka/screens/home/session_list.dart';
-import 'package:barka/services/auth.dart';
 import 'package:barka/services/database.dart';
 import 'package:barka/shared/constants.dart';
 import 'package:flutter/material.dart';
@@ -12,105 +9,24 @@ import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 
 class Home extends StatelessWidget {
-  final AuthService _auth = AuthService();
   @override
   Widget build(BuildContext context) {
-    void _showCreatingNewSessionModal() {
-      showModalBottomSheet(
-          context: context,
-          builder: (context) {
-            return Container(
-              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 60),
-              child: AddingSession(),
-            );
-          });
-    }
-
-    void _showSearchingForSessionModal() {
-      showModalBottomSheet(
-          context: context,
-          builder: (context) {
-            return Container(
-              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 60),
-              child: SearchingSession(),
-            );
-          });
-    }
-
     return StreamProvider<List<Session>>.value(
         value: DatabaseService(uid: Provider.of<User>(context).uid).session,
         child: Scaffold(
-          backgroundColor: Colors.green[50],
           appBar: AppBar(
             shape: MyShapeBorder(20),
             toolbarHeight: 160,
-            elevation: 7,
-            bottom: PreferredSize(
-              preferredSize: Size.fromHeight(0),
-              child: Padding(
-                padding: const EdgeInsets.only(
-                  left: 8,
-                ),
-              ),
-            ),
+            elevation: 0.0,
             centerTitle: true,
             backgroundColor: Colors.green[400],
             title: Text(
               "بركة",
               style: TextStyle(fontSize: 52, fontFamily: 'Aref'),
             ),
-            actions: [
-              Padding(
-                padding: const EdgeInsets.only(left: 20.0, bottom: 50.0),
-                child: IconButton(
-                  onPressed: () async {
-                    await _auth.signOut();
-                  },
-                  icon: Icon(
-                    Icons.settings,
-                    size: 45,
-                  ),
-                ),
-              ),
-            ],
           ),
           drawer: MainDrawer(
             userUid: Provider.of<User>(context).uid,
-            //   child: Padding(
-            //     padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
-            //     child: Column(
-            //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            //       children: [
-            //         FlatButton.icon(
-            //           icon: Icon(
-            //             Icons.add_circle,
-            //             color: Colors.cyan[300],
-            //             size: 40,
-            //           ),
-            //           label: Text('إضافة ختمة جديدة'),
-            //           onPressed: () {
-            //             _showCreatingNewSessionModal();
-            //           },
-            //         ),
-            //         IconButton(
-            //             icon: Icon(
-            //               Icons.search,
-            //               size: 40,
-            //             ),
-            //             onPressed: () {
-            //               _showSearchingForSessionModal();
-            //             }),
-            //         IconButton(
-            //             icon: Icon(
-            //               Icons.settings,
-            //               size: 40,
-            //             ),
-            //             onPressed: () {
-            //               _showSearchingForSessionModal();
-            //             }),
-            //       ],
-            //     ),
-            //   ),
           ),
           body: Container(
             decoration: BoxDecoration(
@@ -118,16 +34,6 @@ class Home extends StatelessWidget {
                     image: AssetImage('assets/holly_symbols.png'),
                     fit: BoxFit.cover)),
             child: SessionList(),
-          ),
-          floatingActionButton: FloatingActionButton(
-            child: Icon(
-              Icons.search,
-              size: 38,
-            ),
-            backgroundColor: Colors.green[500],
-            onPressed: () {
-              _showSearchingForSessionModal();
-            },
           ),
         ));
   }
