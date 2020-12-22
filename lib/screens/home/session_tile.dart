5fc33e11bb08f4e559314f6e814f6b2c93950e2e
mohammedj-sadiq <mohammedj.sadiq@outlook.com>
2020-12-22 15:20:48 +0300

    changing the card design for sessions

--- lib/screens/home/session_tile.dart
+++ lib/screens/home/session_tile.dart
@@ -6,16 +6,80 @@ class SessionTile extends StatelessWidget {
   SessionTile({this.session});
   @override
   Widget build(BuildContext context) {
-    //print(session.name);
-    return Padding(
-      padding: EdgeInsets.all(0.0),
+    Size size = MediaQuery.of(context).size;
+    double width = size.width;
+    double height = size.height;
+    return Container(
+      height: height * 0.11,
+      margin: EdgeInsets.fromLTRB(width * 0.05, height * 0.01, width * 0.05, 0),
       child: Card(
-          margin: EdgeInsets.fromLTRB(20, 8, 20, 0),
-          child: Container(
-            height: 60,
-            width: 400,
-            child: Text(session.name),
-          )),
+        elevation: 8.0,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0.0, 8.0, 8.0, 0.0),
+                  child: Text(session.name),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 2, left: 0),
+                  child: Container(
+                    padding: EdgeInsets.all(6),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Color(0xff937b4c),
+                    ),
+                    child: Text(
+                      '1',
+                      style: TextStyle(color: Colors.white, fontSize: 10),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 8.0, left: 8.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      session.description,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 12.0,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20.0),
+                    child: Container(
+                      child: Text('6'),
+                      width: width / 3,
+                    ),
+                  ),
+                  Container(
+                    child: Icon(
+                      Icons.arrow_forward_ios,
+                      size: 18,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Spacer(),
+            Container(
+              height: height * 0.004,
+              width: (width - (width * 0.15)) * 1,
+              color: Colors.green[800],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
